5a64a32c3b92ed80f608e6c2590d543abf28dcb7
aeldit <aeldit.66@proton.me>
2025-01-17 19:47:51 +0200

    Properly handle Results in the main.rs file

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,44 @@ mod todo;
 use states::States;
 use todo::Todos;
 
-fn get_file_path(states: &mut States) -> String {
+fn get_file_path(states: &mut States) -> Option<String> {
     let home_dir = match my_home() {
         Ok(home_dir) => home_dir,
         Err(error) => panic!("Couldn't get the home dir: {error:?}"),
     };
 
-    let binding = home_dir.unwrap();
-    let home_dir_str = match binding.to_str() {
+    let home_dir_str = match home_dir {
         Some(home_dir_str) => home_dir_str,
         None => panic!("Couldn't get the string from the home dir"),
     };
-
-    let file_path = String::from(home_dir_str) + "/.config/todo-tui/todos.json";
-
-    states.set_file_path(file_path.clone());
-
-    file_path
+    match home_dir_str.to_str() {
+        Some(s) => {
+            let file_path = String::from(s) + "/.config/todo-tui/todos.json";
+            states.set_file_path(file_path.clone());
+            Some(file_path)
+        }
+        None => None,
+    }
 }
 
 fn main() {
     let mut states = States::new();
 
-    let file_path = get_file_path(&mut states);
+    let file_path = match get_file_path(&mut states) {
+        Some(file_path) => file_path,
+        None => return,
+    };
 
     let file_exists = exists(&file_path);
-    if file_exists.is_err() || file_exists.is_ok_and(|v| !v) {
-        match std::fs::File::create(&file_path) {
-            Ok(_) => {}
-            Err(_) => panic!("The file '{}' does not exist", file_path),
-        }
+    if (file_exists.is_err() || file_exists.is_ok_and(|v| !v))
+        && std::fs::File::create(&file_path).is_err()
+    {
+        panic!("The file todos file does not exist and could not be create")
     };
-    let file_contents = read_to_string(&file_path).unwrap(); //.expect("Couldn't read the file");
+    let file_contents = match read_to_string(&file_path) {
+        Ok(file_contents) => file_contents,
+        Err(_) => panic!("Counld read the file '{}'", file_path),
+    };
 
     let mut todos = Todos::new(file_contents, file_path);
 
